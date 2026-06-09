31d3a82de01dc50eb13c1590fb5b5993b7299af2
orssdev <orss.dev@gmail.com>
2026-06-09 00:15:44 -0400

    (nvim): split config into lua/config modules, add w/q/Q/x keybinds

---
+++ nvim/lua/config/keybinds.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = ' '
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>w', vim.cmd.w)
+vim.keymap.set('n', '<leader>q', vim.cmd.q)
+vim.keymap.set('n', '<leader>Q', vim.cmd.qa)
+vim.keymap.set('n', '<leader>x', vim.cmd.x)
