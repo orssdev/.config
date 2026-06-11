5085c7d897dc5264449aa3079895740bd8765309
orssdev <orss.dev@gmail.com>
2026-06-11 01:03:37 -0400

    keybind(nvim): harpoon menu to <leader>h

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -7,7 +7,7 @@ return {
         harpoon:setup()
 
         vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
-        vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set('n', '<leader>h', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
         vim.keymap.set('n', '<C-p>', function() harpoon:list():prev() end)
         vim.keymap.set('n', '<C-n>', function() harpoon:list():next() end)
 
