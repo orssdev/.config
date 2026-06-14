ab242ebba92fb63853978306f601c3beac5a7d71
orssdev <orss.dev@gmail.com>
2026-06-14 17:27:35 -0400

    feat(nvim): yazi as file explore
    replaced netrw with yazi pluggin

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>e', '<cmd>Yazi<cr>')
 vim.keymap.set('n', '<leader>w', vim.cmd.w)
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { silent = true })
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { silent = true })
--- nvim/lua/plugins/utilities.lua
+++ nvim/lua/plugins/utilities.lua
@@ -1,6 +1,20 @@
 return {
     { 'tpope/vim-fugitive' },
     {
+        'mikavilpas/yazi.nvim',
+        version = '*',
+        event = 'VeryLazy',
+        dependencies = {
+            { 'nvim-lua/plenary.nvim', lazy = true },
+        },
+        init = function()
+            vim.g.loaded_netrwPlugin = 1
+        end,
+        opts = {
+            open_for_directories = true,
+        },
+    },
+    {
         'windwp/nvim-autopairs',
         event = 'InsertEnter',
         config = function()
